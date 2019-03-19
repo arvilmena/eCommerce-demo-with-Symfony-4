e7634c9abec1cc222d0fbb74c2fa1ea7a2824aa6
Arvil Mena <arvilmena@gmail.com>
2019-03-19 15:41:09 +0100

    add bootstrap4 to asset dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('global-styles', './assets/scss/global.scss')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
