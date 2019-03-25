aad7725acd2a570f28425723d548f472004051a4
Arvil Mena <arvilmena@gmail.com>
2019-03-25 15:44:41 +0100

    CartController and Frontend

--- src/Controller/Web/CartController.php
+++ src/Controller/Web/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Web;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -15,5 +16,12 @@ class CartController extends AbstractController
         return $this->render('web/cart/index.html.twig', [
             'controller_name' => 'CartController',
         ]);
+    }
+
+    /**
+     * @Route("/web/cart/checkout", name="web_cart_checkout", methods={"POST"})
+     */
+    public function checkout(Request $request) {
+        dd($request->request->all());
     }
 }
